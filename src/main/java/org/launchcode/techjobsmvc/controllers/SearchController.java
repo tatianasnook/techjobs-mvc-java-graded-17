0575db6d6a9b0b7312a554b5420fb8995937813f
Tatiana Snook <tatsnook@gmail.com>
2025-01-19 13:23:06 -0600

    implemented jobTableFragment

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
 import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,12 +29,14 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping(value="results")
-    public String displaySearchResults(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
-        ArrayList<?> jobs;
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
         if (searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()) {
             jobs = JobData.findAll();
+            model.addAttribute("title", "Jobs With All:");
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
         }
         model.addAttribute("columns", columnChoices);
         model.addAttribute("jobs", jobs);
